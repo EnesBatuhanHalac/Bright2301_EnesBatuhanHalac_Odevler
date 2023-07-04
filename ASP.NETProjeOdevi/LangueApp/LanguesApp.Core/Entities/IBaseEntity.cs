d0aff4901e17ac15bdcffcc6b25339eed780af36
ENES BATUHAN HALAÇ <halacenesbatuhan@gmail.com>
2023-07-04 17:07:51 +0300

    04.07.2023

---
+++ ASP.NETProjeOdevi/LangueApp/LanguesApp.Core/Entities/IBaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguesApp.Core.Entities
+{
+    public interface IBaseEntity
+    {
+
+    }
+}
---
+++ ASP.NETProjeOdevi/LangueApp/LanguesApp.MVC/Program.cs
@@ -0,0 +1,38 @@
+using LanguesApp.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//builder.Services.AddDbContext<LanguesAppDbContext>(options =>
+//{
+//    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
+//    b => b.MigrationsAssembly("LanguesApp"));
+//});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
