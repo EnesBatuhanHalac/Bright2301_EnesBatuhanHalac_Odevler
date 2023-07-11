6d8bb0bfdee95b27751f3f1df5aced23dffa7108
ENES BATUHAN HALAÇ <halacenesbatuhan@gmail.com>
2023-07-11 21:52:51 +0300

    güncellemeler yapildi 11.07.2023

---
+++ asama2/abc/LangueApp/LanguesApp.Business/Abstract/ILangueService.cs
@@ -0,0 +1,27 @@
+﻿using LanguesApp.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguesApp.Business.Abstract
+{
+    public interface ILangueService
+    {
+        #region Generic
+        Task CreateAsync(Langue langue);
+        void Update(Langue langue);
+        void Delete(Langue langue);
+        Task<List<Langue>> GetAllAsync();
+        Task<Langue> GetByIdAsync(int id);
+        #endregion
+
+        #region Langue
+
+        Task<List<Langue>> GetAllLangues();
+
+        Task<Langue> GetLangueById(int id);
+        #endregion
+    }
+}
