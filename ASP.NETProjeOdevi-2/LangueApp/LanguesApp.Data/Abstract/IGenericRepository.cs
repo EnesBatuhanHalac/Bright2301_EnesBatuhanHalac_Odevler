430a076d08cd00ecc0c7c4b4712d173b84dc7e82
ENES BATUHAN HALAÇ <halacenesbatuhan@gmail.com>
2023-07-11 11:45:00 +0300

    Güncel calisma

---
+++ ASP.NETProjeOdevi-2/LangueApp/LanguesApp.Data/Abstract/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguesApp.Data.Abstract
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        //crete delete update getall get by ıD
+        Task CreateAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+    }
+}
---
+++ ASP.NETProjeOdevi-2/LangueApp/LanguesApp.Data/Abstract/ITeacherRepository.cs
@@ -0,0 +1,21 @@
+﻿using LanguesApp.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguesApp.Data.Abstract
+{
+    public interface ITeacherRepository:IGenericRepository<Teacher>
+    {
+
+        Task<List<Teacher>> GetAllTeachers();
+         Task<Teacher> GetTeacherById(int id);
+        Task<List<Teacher>> GetActiveOrPassiveTeachers(bool IsActive);
+        Task<List<Teacher>> GetTeachersInHome(bool IsActive);
+        Task<List<Teacher>> GetDeletedOrNotDeletedTeachers(bool IsDeleted);
+
+
+    }
+}
