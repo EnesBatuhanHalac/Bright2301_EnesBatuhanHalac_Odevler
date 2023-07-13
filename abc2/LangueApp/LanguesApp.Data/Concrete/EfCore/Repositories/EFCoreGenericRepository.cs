eee6d3fb6cd3e19a113ac62bf1b0fbb1bebb831d
ENES BATUHAN HALAÇ <halacenesbatuhan@gmail.com>
2023-07-13 22:09:10 +0300

    footer

---
+++ abc2/LangueApp/LanguesApp.Data/Concrete/EfCore/Repositories/EFCoreGenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using LanguesApp.Data.Abstract;
+using LanguesApp.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguesApp.Data.Concrete.EfCore.Repositories
+{
+    public class EFCoreGenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        LanguesAppDbContext _context = new LanguesAppDbContext();
+
+        public async Task CreateAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public Task<List<T>> GetAllAsync()
+        {
+            return _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public void Update(T entity)
+        {
+
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+
+        }
+    }
+}
---
+++ abc2/LangueApp/LanguesApp.MVC/Areas/Admin/Controllers/TeacherController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanguesApp.MVC.Areas.Admin.Controllers
+{
+    public class TeacherController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index([FromRoute(Name ="id")]int id)
+        {
+            return View();
+        }
+    }
+}
