f1388061b83fbb8e3a3299f26705ae64c1c951d4
ENES BATUHAN HALAÇ <halacenesbatuhan@gmail.com>
2023-07-18 22:07:53 +0300

    Admin Panel Eklendi

---
+++ MVC Projeleri/Asama4/LangueApp/LanguesApp.Entity/Concrete/Langue.cs
@@ -0,0 +1,18 @@
+﻿using LanguesApp.Core.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguesApp.Entity.Entities
+{
+    public class Langue:BaseEntity
+    {
+        public string Name { get; set; }
+        public string? About { get; set; }
+        public string? Url { get; set; }
+        public List<LangueTeacher> LangueTeachers { get; set; }
+
+    }
+}
---
+++ MVC Projeleri/Asama4/LangueApp/LanguesApp.MVC/Controllers/TeacherShop.cs
@@ -0,0 +1,34 @@
+﻿using LanguesApp.Business.Abstract;
+using LanguesApp.Entity.Entities;
+using LanguesApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguesApp.MVC.Controllers
+{
+    public class TeacherShop : Controller
+    {
+        private readonly ITeacherService _teacherManager;
+
+        public TeacherShop(ITeacherService teacherManager)
+        {
+            _teacherManager = teacherManager;
+        }
+
+        public async Task<IActionResult> TeacherList(string langueurl=null, string teacherurl=null)
+        {
+            List<Teacher> teacherList = await _teacherManager.GetAllActiveTeachersAsync(langueurl, teacherurl);
+            List<TeacherViewModel> teacherViewModelList=teacherList.Select(b=> new TeacherViewModel
+            {
+                About=b.About,
+                Age=b.Age,
+                Country=b.Country,
+                Email=b.Email,
+                FullName=b.FullName,
+                Id=b.Id,
+                ImageUrl = b.ImageUrl,
+                Phone = b.Phone
+            }).ToList();
+            return View(teacherViewModelList);
+        }
+    }
+}
